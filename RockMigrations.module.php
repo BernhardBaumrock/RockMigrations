d116a417b41263a330a3e08b493a922afd6df02f
Richard Jedlička <jedlicka.r@gmail.com>
2021-12-13 18:50:34 +0100

    Ability to specify more pages with the same name declaratively.

--- RockMigrations.module.php
+++ RockMigrations.module.php
@@ -2492,7 +2492,9 @@ class RockMigrations extends WireData implements Module {
 
     // setup pages
     foreach($config->pages as $name=>$data) {
-      if(is_int($name)) {
+      if(isset($data['name'])) {
+        $name = $data['name'];
+      } elseif(is_int($name)) {
         // no name provided
         $name = uniqid();
       }
